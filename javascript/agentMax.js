f6b6399abd9e0698fb1259c79bf331c001120ea1
cbossut <bossut.clement@gmail.com>
2015-11-13 23:53:15 +0100

    Better protection for maxEnergy

--- javascript/agentMax.js
+++ javascript/agentMax.js
@@ -589,7 +589,7 @@ function sorbet(toggle) {
 }
 
 function sorbetMaxEnergy(m) {
-  if (!m) return
+  if (m <= 0) return
   with(danseDuSorbet) {
     sorbet.growDose *= m / sorbet.maxGrow
     sorbet.consumDose *= m / sorbet.maxGrow
