6d0a39dac5cc7a4f2bbc8d4c66f7a8980c141e6f
Unknown <bossut.clement@gmail.com>
2017-05-25 20:53:55 +0200

    Add framesOn to orchestre.js

--- javascript/orchestre.js
+++ javascript/orchestre.js
@@ -11,6 +11,7 @@ setoutletassist(0,"bang when finished");
 
 var dec = jsarguments[2] || 255,
     val = 255,
+    framesOn = 2,
     lamps = [
       jsarguments[3] || 9,
       jsarguments[4] || jsarguments[3] | 9
@@ -20,7 +21,7 @@ var lists = [],
     banged = []
 for (var i = 1 ; i < inlets ; i++) {
   lists[i] = [i-1]
-  banged[i] = false
+  banged[i] = 0 // 0 : nothing, 1 : dec, 2+: framesOn
 }
 
 var lights = [],
@@ -31,25 +32,31 @@ for (var i = 0 ; i < lamps[0]*lamps[1] ; i++) {
 
 function bang() {
   if (inlet) {
-    if (!banged[inlet]) {
+    if (banged[inlet] != framesOn) {
       nothing = false
-      banged[inlet] = true
+      banged[inlet] = framesOn
       for (var i = 0 ; i < lists[inlet].length ; i++) {
         lights[lists[inlet][i]] += val
       }
     }
   } else if (!nothing) {
     nothing = true
-    for (var i = 1 ; i < inlets ; i++) {
-      banged[i] = false
-    }
     for (var i = 0 ; i < lights.length ; i++) {
       outlet(1, lights[i])
-      if (lights[i] < dec) {
-        lights[i] = 0
-      } else {
-        lights[i] -= dec
-        nothing = false
+    }
+    for (var i = 1 ; i < inlets ; i++) {
+      if (banged[i] > 1) {
+        banged[i]--
+      } else if (banged[i] == 1) {
+        for (var i = 0 ; i < lists[inlet].length ; i++) {
+          lights[lists[inlet][i]] -= dec
+          if (lights[lists[inlet][i]] < 0) {
+            lights[lists[inlet][i]] = 0
+            banged[i] = 0
+          } else {
+            nothing = false
+          }
+        }
       }
     }
     outlet(0, "bang")
@@ -63,4 +70,12 @@ function space(banger, x, y, dx, dy) { //TODO something if out of bounds ( < 1 |
       lists[banger].push(j*lamps[0] + i)
     }
   }
+}
+
+function fOn(i) {
+  framesOn = i
+}
+
+function sub(i) {
+  val = i
 }
